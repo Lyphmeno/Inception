9e26497cc43ccdf0a91516f06bd3fb0b9a602999
lyfmeno <hugolevipro@gmail.com>
2023-05-23 16:19:18 +0200

    update to save - TO DO - Compose Wordpress

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
+if [ ! -d "/var/lib/mysql/$SQL_DATABASE" ]; then
+	service mysql start
+	sleep 5
+	mysql << EOF
+
+	CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;
+	CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
+	GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
+	EOF
+
+	mysqladmin -p$SQL_ROOT_PASSWORD shutdown
+fi
+
 exec "$@"
